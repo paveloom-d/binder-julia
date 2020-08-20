810222d338d23bfca4665c174f3602de7c54015a
Pavel Sobolev <paveloom@mail.ru>
2020-08-20 16:00:52 +0300

    Add `IJulia`.

--- scripts/user/julia/install-julia.sh
+++ scripts/user/julia/install-julia.sh
@@ -8,5 +8,8 @@ wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.0-linux-x86_
 echo '\e[1;36m> Extracting the tarball...\e[0m'
 tar -xf ~/Other/julia/julia*.tar.gz -C ~/Other/julia --strip-components 1
 
-echo '\e[1;36m> Deleting the tarball...\e[0m\n'
+echo '\e[1;36m> Deleting the tarball...\e[0m'
 rm ~/Other/julia/julia*.tar.gz
+
+echo '\e[1;36m> Installing `IJulia`...\e[0m\n'
+julia -e 'using Pkg; Pkg.add("IJulia"); using IJulia' >/dev/null 2>&1
