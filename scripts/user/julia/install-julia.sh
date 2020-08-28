0634ad91a0cd70a7f73efd1fa2ccd4b4a95dd9a7
Pavel Sobolev <Paveloom@mail.ru>
2020-08-28 18:07:13 +0300

    (#12) Update Julia and the base image:
    * Update to Julia `1.5.1`;
    * Let the user's group own `/usr/other/$USER`;
    * Update the base image to the version `0.1.2`.

--- scripts/user/julia/install-julia.sh
+++ scripts/user/julia/install-julia.sh
@@ -3,10 +3,10 @@
 echo '\n\e[1;36mInstalling Julia:\e[0m'
 
 echo '\e[1;36m> Creating a hidden folder for the user...\e[0m'
-sudo mkdir -p /usr/other/$USER && sudo chown -R $USER:$USER /usr/other/$USER
+sudo mkdir -p /usr/other/$USER && sudo chgrp -R $USER /usr/other/$USER && sudo chmod -R g+w /usr/other/$USER
 
 echo '\e[1;36m> Downloading the tarball...\e[0m'
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.0-linux-x86_64.tar.gz -P /usr/other/$USER/julia >/dev/null 2>&1
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.1-linux-x86_64.tar.gz -P /usr/other/$USER/julia >/dev/null 2>&1
 
 echo '\e[1;36m> Extracting the tarball...\e[0m'
 tar -xf /usr/other/$USER/julia/julia*.tar.gz -C /usr/other/$USER/julia --strip-components 1
